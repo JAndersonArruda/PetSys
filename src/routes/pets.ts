80eed051f05dfc4fee4cc81e303f291950204078
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-12-27 17:49:30 -0300

    feat: adding POST router for pet

--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import { pets, petshops } from './store';
 
 
 export interface Pets {
@@ -12,14 +13,12 @@ export interface Pets {
     created_at: Date
 }
 
-const pets: Pets[] = [];
-
 export default function configurePetRoutes(router: Router) {
     router.get('/pets', (req: Request, res: Response) => {
         res.status(200).send(pets);
     });
 
-    router.get('/pets/:id', (req: Request, res: Response, next: NextFunction) => {
+    router.get('/pets/:id', (req: Request, res: Response) => {
         const id  = req.params.id;
         const pet = pets.find((pet) => pet.id === id);
 
@@ -29,5 +28,42 @@ export default function configurePetRoutes(router: Router) {
         }
 
         res.status(200).send(pet);
+    });
+
+    router.post('/pets', (req: Request, res: Response) => {
+        const data = req.body as Pets;
+        const cnpjPetshop = req.headers['cnpj'];
+
+        const newPet: Pets = {
+            id: uuidv4(),
+            name: data.name,
+            type: data.type,
+            description: data.description,
+            vaccinated: data.vaccinated,
+            deadline_vaccination: data.deadline_vaccination,
+            created_at: new Date()
+        }
+
+        if(!newPet) {
+            res.status(500).json({ error: "Failed to create pet" });
+            return;
+        }
+
+        if(!cnpjPetshop) {
+            res.status(400).json({ error: "Missing required header 'cnpj'" });
+            return;
+        }
+
+        const petshop = petshops.find((petshop) => petshop.cnpj === cnpjPetshop);
+
+        if(!petshop) {
+            res.status(400).json({ error: "Petshop not found" });
+            return;
+        }
+
+        petshop.pets.push(newPet);
+
+        pets.push(newPet);
+        res.status(201).send(newPet);
     });
 }
