4ca1fa31842e957b6f99cc5daf798ba404d449c0
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-12-30 12:08:45 -0300

    refactor: adjusting PUT router for pets to work with middlewares

--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -7,6 +7,7 @@ import { pets, petshops } from './store';
 import { verify } from 'crypto';
 import verifyPetById from '../utils/middlewares/verifyPetById';
 import verifyBodyData from '../utils/middlewares/verifyBodyData';
+import requiredBodyData from '../utils/middlewares/requiredBodyData';
 
 export interface Pets {
     id: string,
@@ -52,36 +53,13 @@ export default function configurePetRoutes(router: Router) {
         res.status(201).send(newPet);
     });
 
-    router.put('/pets/:id', (req: Request, res: Response) => {
+    router.put('/pets/:id', checkExistsUserAccount, verifyPetById, requiredBodyData, (req: Request, res: Response) => {
+        const pet = req.pet!;
         const data = req.body as Pets;
-        if (!data || Object.keys(data).length === 0) {
-            res.status(400).json({ error: "Missing required fields" });
-            return;
-        }
-
-        const cnpjPetshop = req.headers['cnpj'];
-        if(!cnpjPetshop) {
-            res.status(400).json({ error: "Missing required header 'cnpj'" });
-            return;
-        }
-
-        const petshop = petshops.find((petshop) => petshop.cnpj === cnpjPetshop);
-        if (!petshop) {
-            res.status(400).json({ error: "Petshop not found" });
-            return;
-        }
-
-        const id = req.params.id;
-        const pet = petshop.pets.find((pet) => pet.id === id);
-        if(!pet) {
-            res.status(400).json({ error: "Pet not found" });
-            return;
-        }
 
         pet.name = data.name;
         pet.type = data.type;
         pet.description = data.description;
-        pet.vaccinated = data.vaccinated;
         pet.deadline_vaccination = data.deadline_vaccination;
 
         res.status(200).json({ messsage: "Update successfy" });
